6be63a88a5a6a3e362a768ba44663e8963129934
joseph <josephtsegen10@gmail.com>
2021-09-03 22:57:46 +0100

    env change

--- src/server.ts
+++ src/server.ts
@@ -11,19 +11,22 @@ if (process.env.NODE_ENV !== "production") {
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const DB_URL = process.env.DATABASE_URL as string;
+const DB_URI = process.env.DATABASE_URI as string;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(path.join(__dirname), "../", "public", "views"));
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static(path.resolve("../", "public")));
-
-/* mongoose.connect(DB_URL);
+mongoose.connect(DB_URI, {
+  //@ts-ignore
+  //@ts-nocheck
+  useNewUrlParser: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("connected to Mongoose....")); */
+db.once("open", () => console.log("connected to Mongoose...."));
 
 app.use("/", indexRouter);
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server running on PORT ${PORT} `));
