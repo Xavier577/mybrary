b4bfe0a0c2233c30c037cad5393bc551ad635f57
joseph <josephtsegen10@gmail.com>
2021-10-08 15:54:45 +0100

    connected to mongoDb

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import path from "path";
 import indexRouter from "./routes";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+const DATABASE_URI = process.env.DATABASE_URI as string;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "views"));
@@ -11,11 +19,13 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+mongoose.connect(DATABASE_URI);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to MongoDB!"));
+
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 8080;
-const hostname = "127.0.0.1";
 
-app.listen(PORT as number, hostname, () =>
-  console.log(`listening on http://${hostname}:${PORT}`)
-);
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
